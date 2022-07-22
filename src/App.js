5e3950fc88b2ca3750ed533a3b7550d155ee40fe
Edaly09 <edalyminthe@gmail.com>
2022-07-22 10:33:51 +0200

    methodes addTask

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            tasks: []
+        }
+    }
+    addTask = (taskTodo)=>{
+
+console.log(taskTodo)
+    }
     render() {
         return (
+            <>
             <div>
                 <h1 className="text-3xl text-purple-400">Hello world!</h1>
             </div>
+            <Form addTask={this.state.addTask}/>
+            </>
         );
     }
 }
