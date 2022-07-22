8715868f9a38948e04edb8b58e290b690645eaf2
JHanslik <julien.hanslik@gmail.com>
2022-07-22 14:39:26 +0200

    modification part done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     addTask = (taskTodo) => {
         let clonedTasks = [...this.state.tasks];
         clonedTasks = [
-            { description: taskTodo, status: "To do" },
+            { description: taskTodo, status: "To do", editing: false },
             ...this.state.tasks,
         ];
         this.setState({
@@ -28,7 +28,43 @@ class App extends React.Component {
             tasks: clonedTasks,
         });
     };
+
+    editTask = (task) => {
+        const clonedTasks = [...this.state.tasks];
+        const index = clonedTasks.indexOf(task);
+        clonedTasks[index].editing = true;
+        this.setState({
+            tasks: clonedTasks,
+        });
+    };
+    handleTaskChange = (e, task) => {
+        const clonedTasks = [...this.state.tasks];
+        const index = clonedTasks.indexOf(task);
+        clonedTasks[index].description = e.target.value;
+        this.setState({
+            tasks: clonedTasks,
+        });
+    };
+    handleStatusChange = (e, task) => {
+        const clonedTasks = [...this.state.tasks];
+        const index = clonedTasks.indexOf(task);
+        clonedTasks[index].status = e.target.value;
+        this.setState({
+            tasks: clonedTasks,
+        });
+    };
+
+    validateEdit = (task) => {
+        const clonedTasks = [...this.state.tasks];
+        const index = clonedTasks.indexOf(task);
+        clonedTasks[index].editing = false;
+        this.setState({
+            tasks: clonedTasks,
+        });
+    };
+
     render() {
+        console.log(this.state.tasks);
         return (
             <div className="md:container">
                 <div>
@@ -42,8 +78,21 @@ class App extends React.Component {
                         <List
                             task={task.description}
                             status={task.status}
+                            editingStatus={task.editing}
                             deleteTask={() => {
                                 this.deleteTask(task);
+                            }}
+                            editTask={() => {
+                                this.editTask(task);
+                            }}
+                            handleTaskChange={(e) => {
+                                this.handleTaskChange(e, task);
+                            }}
+                            handleStatusChange={(e) => {
+                                this.handleStatusChange(e, task);
+                            }}
+                            validateEdit={() => {
+                                this.validateEdit(task);
                             }}
                         />
                     );
--- src/components/List.js
+++ src/components/List.js
@@ -6,17 +6,56 @@ class List extends React.Component {
         // console.log(this.props.task);
         // console.log(this.props.status);
         return (
-            <div className="flex place-items-center">
-                <p className="flex-1 ml-5">{this.props.task}</p>
-                <p className="mx-10">{this.props.status}</p>
-                <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-5 mr-0"
-                    onClick={this.props.deleteTask}
-                >
-                    Delete Task
-                </button>
-            </div>
+            <>
+                {!this.props.editingStatus ? (
+                    <div className="flex place-items-center">
+                        <p className="flex-1 ml-5">{this.props.task}</p>
+                        <p className="mx-10">{this.props.status}</p>
+                        <button
+                            type="button"
+                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-5 mr-0"
+                            onClick={this.props.editTask}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            type="button"
+                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-5 mr-0"
+                            onClick={this.props.deleteTask}
+                        >
+                            Delete Task
+                        </button>
+                    </div>
+                ) : (
+                    <form onSubmit={this.props.validateEdit}>
+                        <div className="flex place-items-center">
+                            <input
+                                className="flex-1 ml-5"
+                                type="text"
+                                value={this.props.task}
+                                onChange={this.props.handleTaskChange}
+                            />
+                            <select
+                                name="status"
+                                onChange={this.props.handleStatusChange}
+                            >
+                                <option value={this.props.status}>
+                                    Select Status
+                                </option>
+                                <option value="To do">To do</option>
+                                <option value="Doing">Doing</option>
+                                <option value="Done">Done</option>
+                            </select>
+                            <button
+                                type="submit"
+                                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-5 mr-0"
+                            >
+                                Validate
+                            </button>
+                        </div>
+                    </form>
+                )}
+            </>
         );
     }
 }
