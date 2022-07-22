6dc1c411fe86e90721b47a5245afb43645cf9cd9
JHanslik <julien.hanslik@gmail.com>
2022-07-22 10:44:03 +0200

    form.js methods implemented

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,34 @@
 import React from "react";
 
 class Form extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            task: "",
+        };
+    }
+
+    handleTaskDescriptionChange = (e) => {
+        this.setState({
+            task: e.target.value,
+        });
+    };
+    handleSubmit = () => {
+        this.props.addTask(this.state.task);
+        this.setState({
+            task: "",
+        });
+    };
     render() {
         return (
             <div>
-                <input type="text" placeholder="Add new task" />
-                <button type="submit"></button>
+                <input
+                    type="text"
+                    placeholder="Add new task"
+                    onChange={this.handleTaskDescriptionChange}
+                />
+                <button type="submit" onSubmit={this.handleSubmit}></button>
             </div>
         );
     }
