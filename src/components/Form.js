af4c1bd09e119dd8b8ee77db940406887cd51c83
JHanslik <julien.hanslik@gmail.com>
2022-07-22 11:34:57 +0200

    exercice done until part 04

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,21 +14,30 @@ class Form extends React.Component {
             task: e.target.value,
         });
     };
-    handleSubmit = () => {
+    handleSubmit = (e) => {
+        e.preventDefault();
         this.props.addTask(this.state.task);
         this.setState({
             task: "",
         });
+        e.target.firstChild.value = "";
     };
     render() {
         return (
             <div>
-                <input
-                    type="text"
-                    placeholder="Add new task"
-                    onChange={this.handleTaskDescriptionChange}
-                />
-                <button type="submit" onSubmit={this.handleSubmit}></button>
+                <form action="" onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Add new task"
+                        onChange={this.handleTaskDescriptionChange}
+                    />
+                    <button
+                        type="submit"
+                        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    >
+                        Add Task
+                    </button>
+                </form>
             </div>
         );
     }
